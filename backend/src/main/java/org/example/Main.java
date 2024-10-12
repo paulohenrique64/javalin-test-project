50b1d90c262755e63b4dbce81004495231926717
paulohenrique64 <paulohenriquelvs20@gmail.com>
2024-10-12 15:44:16 -0300

    adicionando arquivos iniciais

---
+++ backend/src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import io.javalin.Javalin;
+import io.javalin.util.FileUtil;
+
+public class Main {
+    public static void main(String[] args) {
+        var app = Javalin
+        .create(config -> {
+            config.bundledPlugins.enableCors(cors -> {
+                cors.addRule(it -> {
+                    it.anyHost();
+                });
+            });
+        }).start(7070);
+
+        // retornar um objeto em formato json
+        app.get("/user", ctx -> {
+            ctx.json(new User("Paulo Henrique", "paulo@gmail.com", "31983078225"));
+        });
+
+        // gerenciar upload de arquivos
+        app.post("/upload", ctx -> {
+            int fileCount = ctx.uploadedFiles("files").size();
+
+            System.out.printf("%d file uploaded%n", fileCount);
+
+            for (int i = 0; i < fileCount; i++) {
+                System.out.println("\"" + ctx.uploadedFiles("files").get(i).filename() + "\" uploaded");
+            }
+
+            ctx.uploadedFiles("files").forEach(uploadedFile ->
+                FileUtil.streamToFile(uploadedFile.content(), "arquivos-recebidos-do-frontend/" + uploadedFile.filename()));
+        });
+    }
+}
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+firefox ./frontend/index.html
+./backend/mvnw -f ./backend/pom.xml compile exec:java
+
